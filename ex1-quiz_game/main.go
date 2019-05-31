59e6b76341759c843372a134b8187e91c876685b
kylerisse <kylerisse@users.noreply.github.com>
2019-05-30 18:56:06 -0700

    exercise 1 bonuses

--- ex1-quiz_game/main.go
+++ ex1-quiz_game/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"strings"
 	"time"
@@ -17,8 +18,11 @@ type question struct {
 }
 
 func main() {
-	filePath, timeLimit := cliFlags()
+	filePath, timeLimit, isShuffle := cliFlags()
 	questions := csvToQuestions(filePath)
+	if isShuffle {
+		questions = shuffleQuestions(questions)
+	}
 	var score int
 	go gameLoop(questions, &score, timeLimit)
 	time.Sleep(time.Duration(timeLimit) * time.Second)
@@ -31,14 +35,15 @@ func gameOver(score int, total int) {
 }
 
 func gameLoop(questions []question, score *int, timeLimit int) {
-
-	fmt.Printf("Answer the following questions n %d seconds:\n", timeLimit)
-	fmt.Printf("--------------------------------------------\n")
+	fmt.Printf("Answer the following questions in %d seconds:\n", timeLimit)
+	fmt.Printf("---------------------------------------------\n")
 	for i := range questions {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Printf("Question #%d: %v = ", i+1, questions[i].question)
 		a, _ := reader.ReadString('\n')
 		a = strings.Replace(a, "\n", "", -1)
+		a = strings.Trim(a, " ")
+		a = strings.ToLower(a)
 		if strings.Compare(questions[i].answer, a) == 0 {
 			*score++
 		}
@@ -56,22 +61,35 @@ func csvToQuestions(filePath string) []question {
 	var qs []question
 	for {
 		record, err := r.Read()
-		if err == io.EOF || len(record) != 2 {
+		if err == io.EOF {
 			break
+		}
+		if len(record) != 2 {
+			continue
 		}
 		qs = append(qs, question{
 			question: record[0],
-			answer:   record[1],
+			answer:   strings.ToLower(record[1]),
 		})
 	}
 	return qs
 }
 
-func cliFlags() (string, int) {
+func shuffleQuestions(qs []question) []question {
+	for i := range qs {
+		j := rand.Intn(i + 1)
+		qs[i], qs[j] = qs[j], qs[i]
+	}
+	return qs
+}
+
+func cliFlags() (string, int, bool) {
 	var filePath string
 	flag.StringVar(&filePath, "f", "problems.csv", "specify problem file.")
 	var timeLimit int
 	flag.IntVar(&timeLimit, "t", 30, "specify time limit.")
+	var shuffle bool
+	flag.BoolVar(&shuffle, "s", false, "shuffle questions.")
 	var help bool
 	flag.BoolVar(&help, "h", false, "display this message.")
 	flag.Usage = func() {
@@ -87,5 +105,5 @@ func cliFlags() (string, int) {
 		fmt.Printf("Invalid time limit %d\n", timeLimit)
 		os.Exit(1)
 	}
-	return filePath, timeLimit
+	return filePath, timeLimit, shuffle
 }
